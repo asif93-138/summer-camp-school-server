a70aa29e26b85e71cf37a34c5ad3155c66e966bd
asif93-138 <asif93@student.sust.edu>
2024-03-12 22:59:27 +0600

    database integration done!

--- index.js
+++ index.js
@@ -1,12 +1,63 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const test = require('./testData.json');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json())
+
+
+
+
+const uri = "mongodb+srv://summer-camp-school:OaEclFbTpRE34cj4@cluster0.iuweya4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const database = client.db("scsDB");
+    const instructors = database.collection("instructors");
+
+    app.get('/classes', async(req, res) => {
+      const result = await instructors.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/classes/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {insID: id};
+      const result = await instructors.find(query).toArray();
+      res.send(result);
+    })
+
+    app.post('/classes', async(req, res) => {
+      const classData = req.body;
+      const result = await instructors.insertOne(classData);
+      res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 
 app.get('/', (req, res) => {
@@ -27,5 +78,5 @@ app.listen(port, () => {
 })
 
 
-// summer - camp - school
+// summer-camp-school
 // OaEclFbTpRE34cj4
