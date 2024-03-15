d0d4c51854e8f669005b4a63d873ce8c39853003
asif93-138 <asif93@student.sust.edu>
2024-03-15 23:19:34 +0600

    failed to update mongodb data..

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ async function run() {
     await client.connect();
     const database = client.db("scsDB");
     const instructors = database.collection("instructors");
+    const students = database.collection("students");
+    const payments = database.collection("payments");
 
     app.get('/classes', async(req, res) => {
       const result = await instructors.find().toArray();
@@ -41,9 +43,52 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/selections/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {student: id};
+      const result = await students.find(query).toArray();
+      res.send(result);
+    })
+
+    app.post('/selections', async(req, res) => {
+      const courseData = req.body;
+      const result = await students.insertOne(courseData);
+      res.send(result);
+    })
+
     app.post('/classes', async(req, res) => {
       const classData = req.body;
       const result = await instructors.insertOne(classData);
+      res.send(result);
+    })
+
+    app.put('/payments', async(req, res) => {
+      const paymentData = req.body;
+      const id = paymentData.course.cN;
+      console.log(id);
+      console.log(paymentData);
+      const result = await payments.insertOne(paymentData);
+      const filter = { cN: id };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          enrolled: 1
+        },
+      };
+      const result1 = await instructors.updateOne(filter, updateDoc, options);
+      res.send({result, result1});
+    })
+
+    app.put('updateTesting', async(req, res) => {
+      const receivedData = req.body;
+      console.log(receivedData);
+      res.send({"status": "working"})
+    })
+
+    app.delete('/selections/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await students.deleteOne(query);
       res.send(result);
     })
 
